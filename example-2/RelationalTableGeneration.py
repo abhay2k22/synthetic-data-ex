94195207988a9029ebc181e56776592c68cea309
abhay2k22 <36631756+abhay2k22@users.noreply.github.com>
2020-07-12 21:20:46 +0530

    modified

--- example-2/RelationalTableGeneration.py
+++ example-2/RelationalTableGeneration.py
@@ -1,29 +1,31 @@
 from sdv import SDV
 import pandas as pd
 import json
-sdv = SDV()
-users = pd.DataFrame({
-    'user_id': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    'country': ['USA', 'UK', 'ES', 'UK', 'USA', 'DE', 'BG', 'ES', 'FR', 'UK'],
-    'gender': ['M', 'F', None, 'M', 'F', 'M', 'F', None, 'F', None],
-    'age': [34, 23, 44, 22, 54, 57, 45, 41, 23, 30]
-})
-'''
-Original Data
-'''
-print(users)
-tables = {
-    'users': users
-}
-with open('./single_table_metadata.json') as metadata_file:
-    metadata = json.load(metadata_file)
-    sdv.fit(metadata, tables)
-    sdv.save('SingleTablesdv.pkl')
-sdv = SDV.load('./SingleTablesdv.pkl')
-samples = sdv.sample_all(10)
-df = pd.DataFrame(samples['users'])
-'''
-Print Synthetsized Data
-'''
-print(df)
 
+
+if __name__=="__main__":
+    users = pd.DataFrame({
+        'user_id': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        'country': ['USA', 'UK', 'ES', 'UK', 'USA', 'DE', 'BG', 'ES', 'FR', 'UK'],
+        'gender': ['M', 'F', None, 'M', 'F', 'M', 'F', None, 'F', None],
+        'age': [34, 23, 44, 22, 54, 57, 45, 41, 23, 30]
+    })
+    '''
+    Original Data
+    '''
+    tables = {
+        'users': users
+    }
+    sdv = SDV()
+
+    with open('./single_table_metadata.json') as metadata_file:
+        metadata = json.load(metadata_file)
+        sdv.fit(metadata, tables)
+        sdv.save('SingleTablesdv.pkl')
+    sdv = SDV.load('./SingleTablesdv.pkl')
+    samples = sdv.sample_all(10)
+    df = pd.DataFrame(samples['users'])
+    '''
+    Print Synthetsized Data
+    '''
+    print(df)
